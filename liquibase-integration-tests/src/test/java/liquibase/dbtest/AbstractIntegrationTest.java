22a5c2de95669e0487c989b6fe68ff429e3d04a4
Wesley Willard <wwillard@datical.com>
2020-06-03 12:12:31 -0500

    WIP

--- liquibase-integration-tests/src/test/java/liquibase/dbtest/AbstractIntegrationTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/dbtest/AbstractIntegrationTest.java
@@ -180,7 +180,7 @@ public abstract class AbstractIntegrationTest {
         openConnection(getJdbcUrl(), getUsername(), getPassword());
 
         // Do not count the test as successful if we skip it because of a failed login. Count it as skipped instead.
-        org.junit.Assume.assumeTrue(database != null);
+        //org.junit.Assume.assumeTrue(database != null);
 
         if (database != null) {
             if (database.supportsTablespaces()) {
@@ -203,6 +203,9 @@ public abstract class AbstractIntegrationTest {
             LockServiceFactory.getInstance().getLockService(database).init();
 
             ChangeLogHistoryServiceFactory.getInstance().resetAll();
+        }
+        else {
+            logger.info("NO DATABASE OBJECT CREATED");
         }
     }
 
