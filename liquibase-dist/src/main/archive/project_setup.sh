117596f71b8cd23c2c9920ec9231f502648c11f8
SteveZ <szandany@datical.com>
2021-04-20 22:23:09 -0500

    Added Vertica,H2,sampleH2,MariaDB,Redshift,CockroachDB project builders

--- liquibase-dist/src/main/archive/project_setup.sh
+++ liquibase-dist/src/main/archive/project_setup.sh
@@ -5,6 +5,8 @@ PROPERTIES_FILE=""
 JDBC_DRIVER_WGET=""
 CHANGELOG_BODY="--liquibase formatted sql\n"
 CHANGELOG_NAME="changelog.sql"
+PROKEY=""
+APIKEY=""
 
 # -t${installer:liquibase.platformType}
 #   0 = Oracle
@@ -77,7 +79,56 @@ echo "database=$database"
 echo "project=$project"
 
 case $platform in
+   
+   sampleH2 | sampleh2 | 2)
+   if mkdir -p ~/lb_workspace; then echo " ";else echo " "; fi
+   mkdir -p ~/lb_workspace/$project
+   cp -a examples/. ~/lb_workspace/$project
+   echo "Here is your prject location:"
+   tput setaf 3;echo $(dirname ~/lb_workspace/$project/start-h2);tput sgr0
+   echo "For information on using and configuring the h2 database see: 
+https://www.liquibase.org/documentation/tutorials/h2.html"
+   exit
+    ;;
+   
+   Vertica | vertica | 21 )
+
+   JDBC_DRIVER_WGET="lib/vertica.jar https://www.vertica.com/client_drivers/10.0.x/10.0.0-0/vertica-jdbc-10.0.0-0.jar"
+   if mkdir -p ~/lb_workspace; then echo " " ;else echo " ";fi
+   if mkdir -p ~/lb_workspace/extensions; then echo " ";else echo " ";fi
+   EXTENSION_LATEST_VERSION=$(curl -s https://github.com/liquibase/liquibase-vertica/releases/latest | grep -o "vertica-.*" | sed s/'>.*'//g | sed 's/"//g'| sed s/'vertica-'//g)
+   wget -q --no-verbose -O ~/lb_workspace/extensions/liquibase-vertica-${EXTENSION_LATEST_VERSION}.jar https://github.com/liquibase/liquibase-vertica/releases/download/liquibase-vertica-${EXTENSION_LATEST_VERSION}/liquibase-vertica-${EXTENSION_LATEST_VERSION}.jar
+   PROPERTIES_FILE="changeLogFile: ${CHANGELOG_NAME}\nurl: jdbc:vertica://$host:$port/${database}\nusername: $username\npassword: $password\ndriver: com.vertica.jdbc.Driver\nclasspath: ../extensions/liquibase-vertica-${EXTENSION_LATEST_VERSION}.jar\n"
+    ;;
+   
+   MariaDB | mariadb |20)
   
+   JDBC_DRIVER_WGET="lib/mariadb.jar https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/2.6.0/mariadb-java-client-2.6.0.jar"
+   PROPERTIES_FILE="changeLogFile: ${CHANGELOG_NAME}\nurl: jdbc:mariadb://$host:$port/$database\nusername: $username\npassword: $password\n"
+    ;;
+   
+   CockroachDB | cockroachdb | Cockroachdb |20)
+  
+   JDBC_DRIVER_WGET="lib/postgresql.jar https://repo1.maven.org/maven2/org/postgresql/postgresql/42.2.18/postgresql-42.2.18.jar"
+   PROPERTIES_FILE="changeLogFile: ${CHANGELOG_NAME}\nurl: jdbc:postgresql://$host:$port/$database\nusername: $username\npassword: $password\n"
+    ;;
+   
+   Redshift | redshift | 12 )
+
+   JDBC_DRIVER_WGET="lib/redshift.jar https://repository.mulesoft.org/nexus/content/repositories/public/com/amazon/redshift/redshift-jdbc42/1.2.1.1001/redshift-jdbc42-1.2.1.1001.jar"
+   if mkdir -p ~/lb_workspace; then echo " " ;else echo " ";fi
+   if mkdir -p ~/lb_workspace/extensions; then echo " ";else echo " ";fi
+   EXTENSION_LATEST_VERSION=$(curl -s https://github.com/liquibase/liquibase-redshift/releases/latest | grep -o "redshift-.*" | sed s/'>.*'//g | sed 's/"//g'| sed s/'redshift-'//g)
+   wget -q --no-verbose -O ~/lb_workspace/extensions/liquibase-redshift-${EXTENSION_LATEST_VERSION}.jar https://github.com/liquibase/liquibase-redshift/releases/download/liquibase-redshift-${EXTENSION_LATEST_VERSION}/liquibase-redshift-${EXTENSION_LATEST_VERSION}.jar
+   PROPERTIES_FILE="changeLogFile: ${CHANGELOG_NAME}\nurl: jdbc:redshift://$host:$port/${database}\nusername: $username\npassword: $password\nclasspath: ../extensions/liquibase-redshift-${EXTENSION_LATEST_VERSION}.jar"
+    ;;
+   
+   H2 | h2| 17)
+  
+   JDBC_DRIVER_WGET="/dev/null http://blah.meh.com/my/path"
+   PROPERTIES_FILE="changeLogFile: ${CHANGELOG_NAME}\nurl: jdbc:h2:file:./$database\nusername: $username\npassword: $password\n"
+    ;;
+   
    Sybase | sybase | 16)
   
    JDBC_DRIVER_WGET="lib/sybase.jar https://repo1.maven.org/maven2/net/sf/squirrel-sql/plugins/sybase/3.5.0/sybase-3.5.0.jar"
