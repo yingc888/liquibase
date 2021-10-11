4f80b3802cbfbbfdf039eb6fc3ff3ca2113587fa
Surya Aki <saki@datical.com>
2021-10-11 17:10:58 -0500

    use file copy

--- liquibase-core/src/main/java/liquibase/command/core/InitCommandStep.java
+++ liquibase-core/src/main/java/liquibase/command/core/InitCommandStep.java
@@ -6,7 +6,12 @@ import liquibase.exception.CommandExecutionException;
 import liquibase.hub.HubService;
 import liquibase.hub.HubServiceFactory;
 import liquibase.ui.UIService;
+import liquibase.util.FileUtil;
+
 import java.io.PrintWriter;
+import java.io.File;
+import java.nio.file.*;
+import java.io.FileWriter;
 
 public class InitCommandStep extends AbstractCommandStep {
 
@@ -37,10 +42,33 @@ public class InitCommandStep extends AbstractCommandStep {
                 throw new CommandExecutionException("The command registerChangeLog requires communication with Liquibase Hub, \nwhich is prevented by liquibase.hub.mode='off'. \nPlease set to 'all' or 'meta' and try again.  \nLearn more at https://hub.liquibase.com");
             }
 
+            //send hub code and get hub Api Key
+
             String hubCode = commandScope.getArgumentValue(HUB_CODE_ARG);
+            String hubApiKey = "";
+
+            //copy liquibase properties from LIQUIBASE_HOME/examples/liquibase.properties to the current working directory
 
             final HubService service = Scope.getCurrentScope().getSingleton(HubServiceFactory.class).getService();
+            String liquibaseInstallPath = System.getenv("LIQUIBASE_HOME");
+            File examplePropertiesFile = new File(liquibaseInstallPath+"/examples/liquibase.properties");
+            File liquibasePropertiesFile = new File("liquibase.properties");
+            Files.copy(examplePropertiesFile.toPath(), liquibasePropertiesFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
+            //append hubApiKey to liquibase.properties
+
+            File file = new File("liquibase.properties");
+            FileWriter fr = new FileWriter(file, true);
+            fr.write("apiKey="+hubApiKey);
+            fr.close();
+
+            //copy sample.changelog.xml from LIQUIBASE_HOME/examples/sameple.changelog.xml to the current working directory
+
+            File exampleChangelogXml= new File(liquibaseInstallPath+"/examples/sample.changelog.xml");
+            File changelogXmlFile = new File("sample.changelog.xml");
+            Files.copy(exampleChangelogXml.toPath(), changelogXmlFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+            // register changelog
 
         }
     }
